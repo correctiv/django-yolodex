8b3eaa94972466bbc5e0a9256aff7b344c7c1791
Stefan Wehrmeyer <mail@stefanwehrmeyer.com>
2015-07-08 14:11:46 +0200

    Add update network button to realm admin

--- yolodex/admin.py
+++ yolodex/admin.py
@@ -1,12 +1,41 @@
 from django.contrib import admin
+from django.shortcuts import redirect
+from django.conf.urls import url
+from django.utils.translation import ugettext_lazy as _
+from django.core.exceptions import PermissionDenied
 
 from parler.admin import TranslatableAdmin
 
 from .models import Realm, EntityType, Entity, Relationship, RelationshipType
+from .importer import YolodexImporter
 
 
 class RealmAdmin(admin.ModelAdmin):
     prepopulated_fields = {'slug': ('name',)}
+
+    def get_urls(self):
+        urls = super(RealmAdmin, self).get_urls()
+        my_urls = [
+            url(r'^(?P<realm_id>\d+)/update-graph/$',
+                self.admin_site.admin_view(self.update_graph),
+                name='yolodex-admin_update_graph'),
+        ]
+        return my_urls + urls
+
+    def update_graph(self, request, realm_id):
+        if not request.method == 'POST':
+            raise PermissionDenied
+        if not self.has_change_permission(request):
+            raise PermissionDenied
+
+        realm = Realm.objects.get(pk=realm_id)
+        yimp = YolodexImporter(realm)
+        yimp.import_graph_from_urls(
+            realm.node_url, realm.edge_url,
+            update=True,
+        )
+        self.message_user(request, _("Graph updated: %s" % yimp.stats))
+        return redirect('admin:yolodex_realm_change', realm.pk)
 
 
 class EntityTypeAdmin(TranslatableAdmin):
