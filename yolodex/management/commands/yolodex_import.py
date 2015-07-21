db67041baba243cc906cb8360f9442e759ace740
Stefan Wehrmeyer <mail@stefanwehrmeyer.com>
2015-07-21 16:31:11 +0200

    Add URL import to management command

--- yolodex/management/commands/yolodex_import.py
+++ yolodex/management/commands/yolodex_import.py
@@ -25,28 +25,37 @@ class Command(BaseCommand):
     )
 
     def handle(self, *args, **options):
-        realm_slug, nodes_filename, edges_filename = args[:3]
-        media_dir = None
-        if len(args) > 3:
-            media_dir = args[3]
+        realm_slug = args[0]
         realm = Realm.objects.get(slug=realm_slug)
+        yimp = YolodexImporter(realm)
         logging.basicConfig(level=logging.INFO)
         self.stdout.write('Importing graph to %s' % realm)
-        yimp = YolodexImporter(realm)
+        media_dir = None
         kwargs = dict(
             media_dir=media_dir,
             clear=options['clear'],
             update=options['update']
         )
-        if nodes_filename.startswith(('http://', 'https://')):
+
+        if len(args) > 1:
+            nodes_filename, edges_filename = args[1:3]
+            if len(args) > 3:
+                media_dir = args[3]
+            if nodes_filename.startswith(('http://', 'https://')):
+                yimp.import_graph_from_urls(
+                    nodes_filename, edges_filename, **kwargs
+                )
+            else:
+                yimp.import_graph_from_files(
+                    open(nodes_filename), open(edges_filename), **kwargs
+                )
+        elif realm.node_url and realm.edge_url:
             yimp.import_graph_from_urls(
-                nodes_filename, edges_filename, **kwargs
+                realm.node_url, realm.edge_url,
+                **kwargs
             )
         else:
-            yimp.import_graph_from_files(
-                open(nodes_filename), open(edges_filename), **kwargs
-            )
-
+            raise Exception('Not enough arguments/no import urls on realm.')
         self.stdout.write('Import done. Stats: %s' % yimp.stats)
         self.stdout.write('Assigning degrees of %s' % realm)
         yimp.assign_degree()
